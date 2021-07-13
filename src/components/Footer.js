c7cf471e834813807504fd80eb6974ad69d865be
Scott Erwin <scottallenerwin@gmail.com>
2021-07-13 15:44:29 -0400

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// Here we are importing a CSS file as a dependency
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <p>&#xa9;2021 Scott Allen Erwin</p>
+    </footer>
+  );
+}
+
+export default Footer;
