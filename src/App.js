a1a800c4fc13b956179d813fe0978f9f6c6cf7f7
Scott Erwin <scottallenerwin@gmail.com>
2021-07-13 15:17:22 -0400

    added App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
