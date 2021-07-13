814de59a75cd91211fbc98fe495eea897191f557
Scott Erwin <scottallenerwin@gmail.com>
2021-07-13 15:18:13 -0400

    updated app.js with test component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Card from './components/Card';
+import Test from './components/Test';
 
 function App() {
   return (
