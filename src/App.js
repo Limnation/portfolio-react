e2b9409ee9c56187d8d38ccf75be805cd9023951
Scott Erwin <scottallenerwin@gmail.com>
2021-07-13 15:53:14 -0400

    added component imports into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import Test from './components/Test';
+import Test from './components/Header';
+import Test from './components/Aboutme';
+import Test from './components/Skills';
+import Test from './components/Portfolio';
+import Test from './components/Contact';
+import Test from './components/Bottomnav';
+import Test from './components/Footer';
+
 
 function App() {
   return (
