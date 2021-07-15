7647871f741fddedbb9a241941e9277e07aff842
Scott Erwin <scottallenerwin@gmail.com>
2021-07-15 14:57:36 -0400

    updated objects and protfolio hover effects

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -2,76 +2,73 @@
 // Portfolio ojbects
 export const devicesImages = {
   default: {
-    backgroundImage: "url(/images/protfolio/devices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  amg: {
-    backgroundImage: "url(/images/protfolio/amgDevices.png')",
-    backgroundRepeat: "no-repeat",
-  },
-  cig: {
-    backgroundImage: "url(/images/protfolio/cigDevices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  ehrmann: {
-    backgroundImage: "url(/images/protfolio/ehrDevices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  agv: {
-    backgroundImage: "url(/images/protfolio/agvDevices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  oml: {
-    backgroundImage: "url(/images/protfolio/omlDevices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  eci: {
-    backgroundImage: "url(/images/protfolio/eciDevices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  sdc: {
-    backgroundImage: "url(/images/protfolio/sdcDevices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  p1: {
-    backgroundImage: "url(/images/protfolio/p1Devices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-  p2: {
-    backgroundImage: "url(/images/protfolio/p2Devices.png)",
-    backgroundRepeat: "no-repeat",
-  },
-};
-
-export const devicesLinks = {
-  default: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/devices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "#desktop",
   },
   amg: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/amgDevices.png')",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://www.mercedes-amg.com/en/home.html",
   },
   cig: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/cigDevices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://www.cigaretteracing.com/",
   },
   ehrmann: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/ehrDevices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://www.ehrmann.com/home",
   },
   agv: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/agvDevices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://www.agv.com/us/en/",
   },
   oml: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/omlDevices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://www.onemagnolialane.com/",
   },
   eci: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/eciDevices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://www.electricalcontactsint.com/",
   },
   sdc: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/sdcDevices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "http://www.salonedicapelliptc.com/",
   },
   p1: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/p1Devices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://zreggert.github.io/get-movie-information/",
   },
   p2: {
+    devices: {
+      backgroundImage: "url(/images/protfolio/p2Devices.png)",
+      backgroundRepeat: "no-repeat",
+    },
     site: "https://limited-footwear.herokuapp.com/",
   },
 };
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 // Import CSS File Dependency
 import "../styles/homepage.css";
-import { devicesImages, devicesLinks } from "./Objects";
+import { devicesImages } from "./Objects";
 
 export default function Portfolio() {
+  const [portfolioDevices, setportfolioDevices] = useState(
+    devicesImages.default.devices
+  );
+
+  const handleMouseOver = () => {
+    setportfolioDevices(devicesImages.amg.devices);
+  };
+
+  const handleMouseOut = () => {
+    setportfolioDevices(devicesImages.default.devices);
+  };
+
   return (
     <section className="portfolio sectionArea" id="portfolio">
       <div className="portfolioTitle sectionTitle">
@@ -13,80 +25,80 @@ export default function Portfolio() {
       <div className="companies">
         <div className="portfolioImages">
           <img
-            onclick="onclickPtItem1();"
-            onmouseover="onHoverPtItem1On();"
-            onmouseout="onHoverPtItem1Off();"
+            onClick="onclickPtItem1();"
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
             className="portfolio-item1 portfolio-items"
             src="./images/protfolio/amg.png"
             alt="amg"
           />
           <img
-            onclick="onclickPtItem2();"
-            onmouseover="onHoverPtItem2On();"
-            onmouseout="onHoverPtItem2Off();"
+            onClick="onclickPtItem2();"
+            onMouseOver="onHoverPtItem2On();"
+            onMouseOut="onHoverPtItem2Off();"
             className="portfolio-item2 portfolio-items"
             src="./images/protfolio/cig.png"
             alt="Cigarette Racing Team"
           />
           <img
-            onclick="onclickPtItem3();"
-            onmouseover="onHoverPtItem3On();"
-            onmouseout="onHoverPtItem3Off();"
+            onClick="onclickPtItem3();"
+            onMouseOver="onHoverPtItem3On();"
+            onMouseOut="onHoverPtItem3Off();"
             className="portfolio-item3 portfolio-items"
             src="./images/protfolio/ehrmann.png"
             alt="ehrmann"
           />
           <img
-            onclick="onclickPtItem4();"
-            onmouseover="onHoverPtItem4On();"
-            onmouseout="onHoverPtItem4Off();"
+            onClick="onclickPtItem4();"
+            onMouseOver="onHoverPtItem4On();"
+            onMouseOut="onHoverPtItem4Off();"
             className="portfolio-item4 portfolio-items"
             src="./images/protfolio/agv.png"
             alt="agv"
           />
           <img
-            onclick="onclickPtItem5();"
-            onmouseover="onHoverPtItem5On();"
-            onmouseout="onHoverPtItem5Off();"
+            onClick="onclickPtItem5();"
+            onMouseOver="onHoverPtItem5On();"
+            onMouseOut="onHoverPtItem5Off();"
             className="portfolio-item5 portfolio-items"
             src="./images/protfolio/oml.png"
             alt="OML"
           />
           <img
-            onclick="onclickPtItem6();"
-            onmouseover="onHoverPtItem6On();"
-            onmouseout="onHoverPtItem6Off();"
+            onClick="onclickPtItem6();"
+            onMouseOver="onHoverPtItem6On();"
+            onMouseOut="onHoverPtItem6Off();"
             className="portfolio-item6 portfolio-items"
             src="./images/protfolio/eci.png"
             alt="ECI"
           />
           <img
-            onclick="onclickPtItem7();"
-            onmouseover="onHoverPtItem7On();"
-            onmouseout="onHoverPtItem7Off();"
+            onClick="onclickPtItem7();"
+            onMouseOver="onHoverPtItem7On();"
+            onMouseOut="onHoverPtItem7Off();"
             className="portfolio-item7 portfolio-items"
             src="./images/protfolio/sdc.png"
             alt="Salone Di Capelli"
           />
           <img
-            onclick="onclickPtItem8();"
-            onmouseover="onHoverPtItem8On();"
-            onmouseout="onHoverPtItem8Off();"
+            onClick="onclickPtItem8();"
+            onMouseOver="onHoverPtItem8On();"
+            onMouseOut="onHoverPtItem8Off();"
             className="portfolio-item8 portfolio-items"
             src="./images/protfolio/project1.png"
             alt="project 1"
           />
           <img
-            onclick="onclickPtItem9();"
-            onmouseover="onHoverPtItem9On();"
-            onmouseout="onHoverPtItem9Off();"
+            onClick="onclickPtItem9();"
+            onMouseOver="onHoverPtItem9On();"
+            onMouseOut="onHoverPtItem9Off();"
             className="portfolio-item9 portfolio-items"
             src="./images/protfolio/project2.png"
             alt="project 2"
           />
         </div>
       </div>
-      <div className="devices" style={devicesImages.default} id="devices"></div>
+      <div className="devices" style={portfolioDevices} id="devices"></div>
       <nav>
         <div className="downArrowFG">
           <a href="#contact">
